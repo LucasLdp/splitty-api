6d3815acb244a51cf4d199287d21c00bf90e4dc9
LucasLdp <lucasdepauloldp1@gmail.com>
2026-04-01 23:50:56 -0300

    feat: configure Swagger and Zod for API documentation and validation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
+import { cleanupOpenApiDoc } from 'nestjs-zod';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import z from 'zod';
 
 async function bootstrap() {
+  z.config(z.locales.pt());
+
   const app = await NestFactory.create(AppModule);
+
+  const openApiDoc = SwaggerModule.createDocument(
+    app,
+    new DocumentBuilder()
+      .setTitle('Example API')
+      .setDescription('Example API description')
+      .setVersion('1.0')
+      .build(),
+  );
+
+  SwaggerModule.setup('api', app, cleanupOpenApiDoc(openApiDoc));
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+bootstrap()
+  .then(() => {
+    console.log('Server is running on port', process.env.PORT ?? 3000);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
