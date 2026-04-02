9d7b8dc67629b9d9961742e2774aaf230caada21
LucasLdp <lucasdepauloldp1@gmail.com>
2026-04-01 23:49:39 -0300

    feat: integrate Zod validation and serialization in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
-import { Module } from '@nestjs/common';
+import { ZodValidationPipe, ZodSerializerInterceptor } from 'nestjs-zod';
+import { APP_PIPE, APP_INTERCEPTOR, APP_FILTER } from '@nestjs/core';
+import { Module, Provider } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from '@modules/users/users.module';
+import { HttpExceptionFilter } from '@modules/shared/infrastructure/filters/http-exception.filter';
+import { UserService } from './modules/users/infrastructure/service/user.service';
+
+const provider: Provider[] = [
+  {
+    provide: APP_PIPE,
+    useClass: ZodValidationPipe,
+  },
+  {
+    provide: APP_INTERCEPTOR,
+    useClass: ZodSerializerInterceptor,
+  },
+  {
+    provide: APP_FILTER,
+    useClass: HttpExceptionFilter,
+  },
+];
 
 @Module({
-  imports: [],
+  imports: [UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ...provider, UserService],
 })
 export class AppModule {}
